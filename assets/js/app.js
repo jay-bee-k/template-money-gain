c40ffac66e6b0922350ddced9e3f4c1e3c732aca
Jaybee-k <79182784+JayBee-K@users.noreply.github.com>
2022-10-16 19:44:17 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,27 @@
 ;(function ($) {
-	'use strict';
-	let windowWidth = $(window).width();
+    'use strict';
+    let windowWidth = $(window).width();
 
-	$(function () {
-	});
+    let initSliderReview = function () {
+        if ($('#slider-review').length) {
+            new Swiper('#slider-review .swiper', {
+                speed: 1000,
+                spaceBetween: 30,
+                loop: 0,
+                pagination: {
+                    el: '.slider-theme_actions--pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    1200: {
+                        slidesPerView: 3,
+                    },
+                },
+            });
+        }
+    }
+
+    $(function () {
+        initSliderReview();
+    });
 })(jQuery);
