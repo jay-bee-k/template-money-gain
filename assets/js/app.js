f64e4b34120d3f703f3afc97d8249f6f5e4fcc22
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-10-17 17:20:47 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,45 @@
 ;(function ($) {
-    'use strict';
-    let windowWidth = $(window).width();
+	'use strict';
+	let windowWidth = $(window).width();
+	let initSliderArtcile = function () {
+		if ($('#slider-article').length) {
+			new Swiper('#slider-article .swiper', {
+				speed: 1000,
+				spaceBetween: 30,
+				loop: 0,
+				navigation: {
+					nextEl: '#slider-article .slider-theme_actions--next',
+					prevEl: '#slider-article .slider-theme_actions--prev',
+				},
+				breakpoints: {
+					1200: {
+						slidesPerView: 4,
+					},
+				},
+			});
+		}
+	}
+	let initSliderReview = function () {
+		if ($('#slider-review').length) {
+			new Swiper('#slider-review .swiper', {
+				speed: 1000,
+				spaceBetween: 30,
+				loop: 0,
+				pagination: {
+					el: '.slider-theme_actions--pagination',
+					clickable: true,
+				},
+				breakpoints: {
+					1200: {
+						slidesPerView: 3,
+					},
+				},
+			});
+		}
+	}
 
-    let initSliderReview = function () {
-        if ($('#slider-review').length) {
-            new Swiper('#slider-review .swiper', {
-                speed: 1000,
-                spaceBetween: 30,
-                loop: 0,
-                pagination: {
-                    el: '.slider-theme_actions--pagination',
-                    clickable: true,
-                },
-                breakpoints: {
-                    1200: {
-                        slidesPerView: 3,
-                    },
-                },
-            });
-        }
-    }
-
-    $(function () {
-        initSliderReview();
-    });
+	$(function () {
+		initSliderArtcile();
+		initSliderReview();
+	});
 })(jQuery);
